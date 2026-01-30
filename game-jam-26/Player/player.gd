7707c11cda60c8338fb01422ea113003b14f2806
Mikha <mikharohrs@gmail.com>
2026-01-30 21:25:31 +0000

    Added Dialogue Manager asset by Nathan Hoad

--- game-jam-26/Player/player.gd
+++ game-jam-26/Player/player.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 @export var SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _process(delta: float) -> void:
+	# Interact
+	# CURRENTLY JUST USED TO TEST DIALOGUE
+	if Input.is_action_pressed("Interact"):  
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/test_dialogue.dialogue"), "start")
+
 func _physics_process(delta: float) -> void:
 	#Move player up or down
 	var vert_direction := Input.get_axis("Up","Down")
