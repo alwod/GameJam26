4c641fbc6c644dc2a5b258b1b2739f77e6e62b72
Mikha <mikharohrs@gmail.com>
2026-01-31 15:11:06 +0000

    Added black screen

--- game-jam-26/World/intro_scene.gd
+++ game-jam-26/World/intro_scene.gd
@@ -1,13 +1,32 @@
 extends Node2D
+@onready var black_screen: Sprite2D = $BlackScreen
+@onready var victim: Node2D = $Victim
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#GameState.is_talking = true
-	#DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/intro.dialogue"), "start")
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if GameState.intro_lights_out:
+		lights_out()
+		
+	if GameState.intro_lights_on:
+		lights_on_again()
+		
+	if GameState.intro_resume_music:
+		resume_music()
+
+func lights_out() -> void:
+	black_screen.visible = true
+	GameState.intro_lights_out = false
+	
+func lights_on_again() -> void:
+	black_screen.visible = false
+	victim.global_rotation_degrees = 90.0
+	GameState.intro_lights_on = false
+	
+func resume_music() -> void:
+	GameState.intro_resume_music = false
